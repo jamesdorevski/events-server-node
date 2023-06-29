f265ebf069e2cefc7d22d53fc25da92e0d1de8a6
James Dorevski <james@jamesdorevski.com>
2023-06-30 05:55:54 +1000

    added eslint

--- library/hash.js
+++ library/hash.js
@@ -1,4 +1,4 @@
-const argon2 = require('argon2');
+const argon2 = require("argon2");
 
 function hashPassword(password) {
   try {
@@ -17,6 +17,6 @@ function verifyPassword(hash, password) {
 }
 
 module.exports = {
-    hashPassword,
-    verifyPassword
-}
+  hashPassword,
+  verifyPassword,
+};
